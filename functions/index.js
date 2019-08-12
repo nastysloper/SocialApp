ce878592ef5e296ef3b5b831051135a0168c8ed5
Rich Vogt <richvogtjr@gmail.com>
2019-08-11 21:11:58 -0400

    add the scream id and sort the collection by date

--- functions/index.js
+++ functions/index.js
@@ -15,11 +15,17 @@ app.get('/screams', (req, res) => {
 	admin
 		.firestore()
 		.collection('screams')
+		.orderBy('createdAt', 'desc')
 		.get()
 		.then((data) => {
 			let screams = [];
 			data.forEach((doc) => {
-				screams.push(doc.data());
+				screams.push({
+					screamId: doc.id,
+					body: doc.data().body,
+					userHandle: doc.data().userHandle,
+					createdAt: doc.data().createdAt
+				});
 			});
 			return res.json(screams);
 		})
