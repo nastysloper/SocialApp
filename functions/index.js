f1a599bdba926a00594916c884866f0bf3b75d2a
Rich Vogt <richvogtjr@gmail.com>
2019-08-11 20:44:16 -0400

    get and create routes working

---
+++ functions/index.js
@@ -0,0 +1,53 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+var serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: 'https://socialapp-951f9.firebaseio.com/'
+});
+	
+const express = require('express');
+const app = express();
+
+app.get('/screams', (req, res) => {
+	admin
+		.firestore()
+		.collection('screams')
+		.get()
+		.then((data) => {
+			let screams = [];
+			data.forEach((doc) => {
+				screams.push(doc.data());
+			});
+			return res.json(screams);
+		})
+		.catch((err) => console.error(err));
+});
+
+app.post('/screams', (req, res) => {
+	const newScream = {
+		body: req.body.body,
+		userHandle: req.body.userHandle,
+		createdAt: admin.firestore.Timestamp.fromDate(new Date())
+	};
+
+	admin
+		.firestore()
+		.collection('screams')
+		.add(newScream)
+		.then((doc) => {
+			return res.json({ message: `document ${doc.id} created successfully`});
+		})
+		.catch((err) => {
+			res.status(500).json({ error: 'something went wrong'});
+			console.error(err);
+		});
+});
+
+// you want a base url.
+// then you want base_url/api for an api.
+// This automatically turns into multiple routes.
+
+exports.api = functions.https.onRequest(app);
